4672ca89c599437b48f93471be726e7c9af1169b
Matita <matitald@gmail.com>
2015-11-22 00:21:16 +0100

    added .md to filename. added saved date to front matter

--- public/js/bookmarklet.js
+++ public/js/bookmarklet.js
@@ -11,10 +11,11 @@ var frontMatter = '---' +
   '\r\ntitle: ' + title +
   '\r\ndescription: ' + description +
   '\r\nlink: ' + window.location +
+  '\r\nsaved: ' + formatDate() +
   '\r\n---';
 
 
-window.open('https://github.com/matita/min-gh-jekyll/new/gh-pages/?filename=_posts/' + encodeURIComponent(filename) + '&value=' + encodeURIComponent(frontMatter));
+window.open('https://github.com/matita/min-gh-jekyll/new/gh-pages/?filename=_posts/' + encodeURIComponent(filename) + '.md&value=' + encodeURIComponent(frontMatter));
   
 
 
@@ -24,6 +25,16 @@ function getFileName(title) {
     pad(now.getMonth() + 1) + '-' +
     pad(now.getDate()) + '-' +
     (title || '').toLowerCase().replace(/\W+/g, '-');
+}
+
+function formatDate(date) {
+  date = date || new Date();
+  return date.getFullYear() + '-' +
+    pad(date.getMonth() + 1) + '-' +
+    pad(date.getDate()) + ' ' +
+    pad(date.getHours()) + ':' +
+    pad(date.getMinutes()) + ':' +
+    pad(date.getSeconds());
 }
 
 function pad(num) {
