ae0490c7495abe6e645c2fb7460d475cc7bb5b2a
Matita <matitald@gmail.com>
2015-11-22 19:40:07 +0100

    replaced get html algorithm

--- public/js/bookmarklet.js
+++ public/js/bookmarklet.js
@@ -15,7 +15,19 @@ var frontMatter = '---' +
   '\r\n---';
 
 
-window.open('https://github.com/matita/min-gh-jekyll/new/gh-pages/?filename=_posts/' + encodeURIComponent(filename) + '.md&value=' + encodeURIComponent(frontMatter));
+var parent = getContentHtml();
+console.log('containers', parent);
+//window.open('https://github.com/matita/min-gh-jekyll/new/gh-pages/?filename=_posts/' + encodeURIComponent(filename) + '.md&value=' + encodeURIComponent(frontMatter));
+
+var w = window.contentW || window.open();
+w.document.head.innerHTML = '<style>' +
+  'body { font-family: Georgia; font-size: 20px; max-width: 800px; margin: 0 auto; color: #333; line-height: 1.6; padding-top: 2em; padding-bottom: 2em; }' + 
+  'a { color: #6a9fb5; text-decoration: none; }' +
+  'a:hover { text-decoration: underline; }' +
+  '</style>';
+
+w.document.body.innerHTML = '<h1>' + title + '</h1>' +
+  parent.innerHTML;
   
 
 
@@ -45,4 +57,35 @@ function getMetaValue(name, nameAttr) {
   nameAttr = nameAttr || 'name';
   var meta = document.querySelector('head meta[' + nameAttr + '="' + name + '"]');
   return meta && meta.getAttribute('content') || null;
+}
+
+
+function getContentHtml() {
+  var ps = document.getElementsByTagName('p');
+  var parents = [];
+  var contents = [];
+  var maxLength = 0;
+  var maxIndex = 0;
+
+  for (var i = 0; i < ps.length; i++) {
+    var p = ps[i];
+    var parent = p.parentNode;
+    var parentIndex = parents.indexOf(parent);
+    if (parentIndex == -1) {
+      parentIndex = parents.length;
+      parents.push(parent);
+    }
+
+    var parentPs = contents[parentIndex];
+    if (!parentPs)
+      parentPs = contents[parentIndex] = [];
+    
+    parentPs.push(p);
+    if (parentPs.length > maxLength) {
+      maxLength = parentPs.length;
+      maxIndex = parentIndex;
+    }
+  }
+  
+  return parents[maxIndex];
 }
