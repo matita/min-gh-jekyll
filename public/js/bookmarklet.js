949daa1268d6a420a5479a892554d8eafab38fc4
Matita <matitald@gmail.com>
2015-11-24 11:33:08 +0100

    better style for pre tags

--- public/js/bookmarklet.js
+++ public/js/bookmarklet.js
@@ -28,7 +28,7 @@ w.document.head.innerHTML = '<style>' +
   'a[href]:hover { text-decoration: underline; }' +
   'img { max-width: 100%; height: auto; display: block; margin: 0 auto; text-align: center }' +
   'iframe { max-width: 100%; display: block; margin: 0 auto; }' +
-  'pre { padding: 1em; background: #eee; }'
+  'pre { padding: 1em; background: #eee; font-size: 14px; overflow: auto; }'
   '</style>';
 
 html = '<h1 style="border-bottom: 1px solid #eee; padding-bottom: 1em; margin-bottom: 1em;">' + title + '</h1>' +
