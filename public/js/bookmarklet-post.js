1291c71dee65f09944bc2dbaf1a44cea0b0736c2
Matita <matitald@gmail.com>
2015-12-05 18:42:49 +0100

    more debug on post message

---
+++ public/js/bookmarklet-post.js
@@ -0,0 +1,11 @@
+javascript:(function(){
+  var u = 'http://matita.github.io/min-gh-jekyll/acquire?origin='+encodeURIComponent(location.href);
+  var w = window.open(u);
+  window.addEventListener('message', function(e) {
+    alert('message received');
+    console.log('message', e);
+    if (u.indexOf(e.origin) == -1)
+      return;
+    w.postMessage({test:'some data'}, u);
+  }, false);
+})()
