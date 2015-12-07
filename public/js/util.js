d1e7d6f551f355514c41fc8e62bd2b699948cedc
Matita <matitald@gmail.com>
2015-12-07 16:01:07 +0100

    helper function to buttons

---
+++ public/js/util.js
@@ -0,0 +1,18 @@
+window.util = (function() {
+
+  function actionBtn(selector, action, loadingText) {
+    var btn = document.querySelector(selector);
+    var parent = btn.parentNode;
+    var fn = function() { parent.replaceChild(btn, s); };
+    var s = document.createElement('span');
+    s.innerHTML = loadingText;
+
+
+    btn.addEventListener('click', function() {
+      parent.replaceChild(s, btn);
+
+      action(fn);
+    }, false);
+  }
+
+})();
