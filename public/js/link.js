62143f349b1cd90e760c4f5873c6620bbbd9af51
Matita <matitald@gmail.com>
2015-12-15 11:26:25 +0100

    wrapped archive action with github api

--- public/js/link.js
+++ public/js/link.js
@@ -4,18 +4,34 @@ function Link(link) {
 
   emitonoff(link);
 
+  var repo = util.getRepo();
+  var branch = 'gh-pages';
+
   var proto = {
     isArchived: function() { return false; },
 
     save: function() {
-      link.emit('beforesave', link);
-      setTimeout(function() {
-        link.emit('saved', link);
-      }, 1000);
+      link.emit('beforesave');
+
+      if (!repo)
+        setTimeout(function() { link.emit('save', 'GitHub user not authenticated'); }, 0)
+      else {
+        var commitMessage = 'Saved from ' + link.url;
+        repo.write(branch, '_posts/' + link.filename, link.frontMatter + '\n' + link.content, commitMessage, function(err) {
+          link.emit('save', err);
+        });
+      }
+
       return link; 
     },
 
-    archive: function() { return link; },
+    archive: function() {
+      link.emit('beforearchive');
+      repo.move(branch, link.path, link.path.replace('_posts', '_posts/archive'), function(err) {
+        link.emit('archive');
+      });
+      return link; 
+    },
 
     delete: function() { return link; }
   };
--- public/js/view/LinkActions.js
+++ public/js/view/LinkActions.js
@@ -1,16 +1,36 @@
 function linkActions(node, link) {
 
+  // save
   link.on('beforesave', function() {
     node.classList.add('gh-saving');
   });
 
-  link.on('saved', function() {
+  link.on('save', function(err) {
     node.classList.remove('gh-saving');
+    if (err)
+      alert(err);
   });
 
   node.querySelector('.gh-save').addEventListener('click', function(e) {
     link.save();
     e.preventDefault();
     return false;
+  }, false);
+
+  // archive
+  link.on('beforearchive', function() {
+    node.classList.add('gh-archiving');
   });
+
+  link.on('archive', function(err) {
+    node.classList.remove('gh-archiving');
+    if (err)
+      alert(err);
+  });
+
+  node.querySelector('.gh-archive').addEventListener('click', function(e) {
+    link.archive();
+    e.preventDefault();
+    return false;
+  }, false);
 }
